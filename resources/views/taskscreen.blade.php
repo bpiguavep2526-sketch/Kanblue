9e08b792acc0184cca7d924a1ccf1e1e6125e607
Eric Peralta <eperaltas2324@politecnics.barcelona>
2025-10-09 18:50:20 +0200

    Nav bar template

---
+++ resources/views/taskscreen.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.base')
+
+@section('navbar')
+<nav class="navbar bg-body-tertiary" background-color="#79AAB7">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" font-family="Poppins, Semibold">
+      <img src="public/images/nav_icon.png" alt="Kanblue Logo" width="30" height="24" class="d-inline-block align-text-top">
+      Kanblue
+    </a>
+  </div>
+</nav>
+@endsection

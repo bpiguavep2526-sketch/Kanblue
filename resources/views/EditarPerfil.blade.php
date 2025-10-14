5497a9c923450b2a8c21dbe0649acc54793dca7c
bpiguavep2526-sketch <bpiguavep2526@politecnics.barcelona>
2025-10-14 20:21:35 +0200

    modificaciones

--- resources/views/EditarPerfil.blade.php
+++ resources/views/EditarPerfil.blade.php
@@ -1,38 +1,34 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Perfil de Usuario</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Perfil de Usuario - KanBlue</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/EditarPerfil.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/perfil.css') }}">
 </head>
 <body>
-    
-  <div class="container-box">
-    <div class="card-custom mt-3">
-      <h5 class="mb-3">INFORMACIÓN DEL PERFIL</h5>
-      <button class="btn-logout float-end mb-3">CERRAR SESIÓN</button>
 
-      <form>
-        <div class="mb-3">
-          <label>Email</label>
-          <input type="email" class="form-control" placeholder="Ingresa tu email">
-        </div>
-        <div class="mb-3">
-          <label>Username</label>
-          <input type="text" class="form-control" placeholder="Nombre de usuario">
-        </div>
-        <div class="mb-3">
-          <label>Contraseña</label>
-          <input type="password" class="form-control" placeholder="Nueva contraseña">
-        </div>
-        <div class="mb-3">
-          <label>Confirmar contraseña</label>
-          <input type="password" class="form-control" placeholder="Repite la contraseña">
-        </div>
-        <button type="submit" class="btn-guardar">Guardar cambios</button>
-      </form>
+  <div class="container-box">
+        <div class="container-layer">
+         <div class="card-custom">
+          <div class="perfil-header d-flex align-items-center justify-content-between mb-4">
+            <h5 class="perfil-title m-0 flex-fill text-center">INFORMACIÓN DEL PERFIL</h5>
+              <button class="btn-logout flex-fill ms-3">CERRAR SESIÓN</button>
+          </div>
+
+               <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Correo electrinico</label>
+               <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+             <div class="mb-3 row">
+                <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña</label>
+              <div class="col-sm-10">
+                  <input type="password" class="form-control" id="inputPassword">
+                 </div>
+              </div>
+        
+      </div>
     </div>
   </div>
 

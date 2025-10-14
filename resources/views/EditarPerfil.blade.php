eb9c549ccc8e9c8e3628d86dc92388008a148582
bpiguavep2526-sketch <bpiguavep2526@politecnics.barcelona>
2025-10-14 20:23:47 +0200

    modificaciones otra vez

--- resources/views/EditarPerfil.blade.php
+++ resources/views/EditarPerfil.blade.php
@@ -17,16 +17,20 @@
               <button class="btn-logout flex-fill ms-3">CERRAR SESIÓN</button>
           </div>
 
-               <div class="mb-3">
-              <label for="exampleFormControlInput1" class="form-label">Correo electrinico</label>
-               <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                </div>
-             <div class="mb-3 row">
-                <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña</label>
-              <div class="col-sm-10">
-                  <input type="password" class="form-control" id="inputPassword">
-                 </div>
-              </div>
+              <div class="mb-3 row">
+  <label for="exampleFormControlInput1" class="col-sm-2 col-form-label">Correo electrónico</label>
+  <div class="col-sm-10">
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+  </div>
+</div>
+
+<div class="mb-3 row">
+  <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña</label>
+  <div class="col-sm-10">
+    <input type="password" class="form-control" id="inputPassword">
+  </div>
+</div>
+
         
       </div>
     </div>

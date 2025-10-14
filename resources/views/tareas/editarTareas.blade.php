201c2f26c42d13f49eb0ae21833e850e55e3851f
Jeancarlos Félix Guevara <160491841+jfelixg123@users.noreply.github.com>
2025-10-14 20:13:45 +0200

    cambios
    Cambios en editarTareas, vista, ruta, css, php, html

---
+++ resources/views/tareas/editarTareas.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset('../resources/css/editarTareas.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Editar tarea</title>
+</head>
+
+<body>
+    <div class="card" style="width: 89rem; background-color: #457582;">
+        <div class="card-body">
+            <div class="card" style="width: 86rem; background-color: #79AAB7;">
+                <div class="card-body">
+                    <div class="card" style="width: 84rem; background-color: #EDEDED;">
+                        <div class="card-body">
+                            <form>
+                                <fieldset>
+                                    <legend>TAREA #2</legend>
+                                    <div class="mb-3">
+                                        <label for="disabledSelect" class="form-label">Asignado a: </label>
+                                        <select id="disabledSelect" class="form-select" holder="Usuario #32">
+                                            <option>Disabled select</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="disabledSelect" class="form-label">Tipo de tarea: </label>
+                                        <select id="disabledSelect" class="form-select" holder="Usuario #32">
+                                            <option style="background-color: #F0E73D">Diseño</option>
+                                            <option style="background-color: #7CA3F7">Desarrollo Web</option>
+                                            <option style="background-color: #5BF778">Configuración</option>
+                                            <option style="background-color: #EF6B54">Documentacion</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleFormControlTextarea1" class="form-label">Descripción</label>
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="7" placeholder="Escribe aquí...."></textarea>
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="disabledSelect" class="form-label">Estado</label>
+                                        <select id="disabledSelect" class="form-select" holder="TODO">
+                                            <option>Todo</option>
+                                            <option>Acabado</option>
+                                            <option>En Progreso</option>
+                                        </select>
+                                    </div>
+                                    <div class="mb-3">
+                                        <button type="submit" class="btn btn-danger">ELIMINAR</button>
+                                    </div>
+                                    <div class="mb-3">
+                                        <button type="submit" class="btn btn-primary">EDITAR</button>
+                                    </div>
+                                </fieldset>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <img src="{{ asset('img/formasGeometricas.png') }}" alt="Formas geométricas" class="img-esquina" width="200" height="200">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KanblueProfileController;
+use App\Http\Controllers\TareasController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/perfil',[KanblueProfileController::class,'profile']);
+Route::get('/editarTarea',[TareasController::class,'index']);

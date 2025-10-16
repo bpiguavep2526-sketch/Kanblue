b62185ce7639be3ff95807120ebb599e77b1055d
bpiguavep2526-sketch <bpiguavep2526@politecnics.barcelona>
2025-10-16 19:20:11 +0200

    reditar

--- resources/views/EditarPerfil.blade.php
+++ resources/views/EditarPerfil.blade.php
@@ -14,15 +14,25 @@
          <div class="card-custom">
           <div class="perfil-header d-flex align-items-center justify-content-between mb-4">
             <h5 class="perfil-title m-0 flex-fill text-center">INFORMACIÓN DEL PERFIL</h5>
-              <button class="btn-logout flex-fill ms-3">CERRAR SESIÓN</button>
+              <button class="btn-logout flex-fill ms-3 text-dar">CERRAR SESIÓN</button>
           </div>
-
-              <div class="mb-3 row">
-  <label for="exampleFormControlInput1" class="col-sm-2 col-form-label">Correo electrónico</label>
+           <form>
+              <div class="mb-3 row align-items-center">
+  <label for="exampleFormControlInput1" class="col-sm-2 col-form-label d-flex align-items-center">
+    <img src="{{ asset('assets/correo.png') }}" alt="icono correo" class="me-2" style="width:20px; height:20px;">
+    Correo electrónico
+  </label>
   <div class="col-sm-10">
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+    <input type="email" class="form-control" id="exampleFormControlInput1">
   </div>
 </div>
+
+<div class="mb-3 row">
+    <label for="inputUsuario" class="col-sm-2 col-form-label">Usuario</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputUsuario" >
+    </div>
+  </div>
 
 <div class="mb-3 row">
   <label for="inputPassword" class="col-sm-2 col-form-label">Contraseña</label>
@@ -30,7 +40,14 @@
     <input type="password" class="form-control" id="inputPassword">
   </div>
 </div>
+<div class="mb-3 row">
+    <label for="inputConfirmPassword" class="col-sm-2 col-form-label">Confirmar contraseña</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputConfirmPassword">
+    </div>
+  </div>
 
+</form>
         
       </div>
     </div>

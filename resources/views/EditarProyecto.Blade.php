6ffd18775c6eeefd0f64daa37bb4a06b4329c4a6
bpiguavep2526-sketch <bpiguavep2526@politecnics.barcelona>
2025-10-23 20:20:01 +0200

    editar proyecyos diseño

--- resources/views/EditarProyecto.Blade.php
+++ resources/views/EditarProyecto.Blade.php
@@ -2,10 +2,10 @@
 
 @section('navbar')
   <div class="d-flex align-items-center ms-auto">
-    <span class="text-white small me-3" style="font-family: 'Poppins', sans-serif; font-weight: 300; font-size: 20px">PROYECTO #45</span>
+    <span class="text-white small me-3" style="font-family: 'Poppins', sans-serif; font-weight: 300; font-size: 20px">PROYECTO NUEVO</span>
     <div class="vr text-white me-3" style="height: 50px;"></div>
     <a href="" >    
-      <img src="images/return.png.png" alt="Volver" width="30" height="30">
+      <img src="images/cerrar-sesion.png" alt="Volver" width="30" height="30">
     </a>
   </div>
 @endsection
@@ -13,30 +13,55 @@
 @section('content')
 <main class="main-editarperfil">
   <div class="container-box">
-  <div class="container-layer">
-    <div class="card-custom">
-      <form>
-        <div class="mb-9">
-          <input type="text" class="form-control" placeholder=" Escribe el nombre del proyecto">
+    <div class="container-layer">
+      <div class="card-custom">
+        <div class="perfil-header d-flex align-items-center justify-content-between mb-4">
+          <h5 class="perfil-title m-0 flex-fill text-center">EDITAR PROYECTO</h5>
+          
         </div>
 
-        <div class="mb-5 row align-items-center">
-          <label class="form-label">Añade descripción</label>
-          <textarea class="form-control" rows="3" placeholder="Describe el proyecto..."></textarea>
-        </div>
+        <form class="form-card">
+          <!-- Nombre del proyecto -->
+          <div class="mb-5 row align-items-center">
+            <label class="col-md-3 col-form-label d-flex align-items-center">
+              <img src="{{ asset('images/gestion-de-proyectos.png') }}" alt="icono proyecto" class="me-2" style="width:33px; height:33px;">
+              Nombre del proyecto
+            </label>
+            <div class="col-md-9">
+              <input type="text" class="form-control uniform-input" placeholder="Escribe el nombre del proyecto">
+            </div>
+          </div>
 
-        <div class="form-check mb-3">
-          <input class="form-check-input" type="checkbox" id="checkEmpresa">
-          <label class="form-check-label" for="checkEmpresa">Guardar</label>
-        </div>
+          <!-- Descripción del proyecto -->
+          <div class="mb-5 row align-items-start">
+            <label class="col-md-3 col-form-label d-flex align-items-center ">
+              <img src="{{ asset('images/fuente.png') }}" alt="icono descripción" class="me-2" style="width:33px; height:33px;">
+              Descripción
+            </label>
+            <div class="col-md-9">
+              <textarea class="form-control " rows="3" placeholder="Describe el proyecto..."></textarea>
+            </div>
+          </div>
 
-        <div class="d-flex justify-content-between">
-          <button type="submit" class="btn-guardar">GUARDAR</button>
-          <button type="button" class="btn-eliminar">ELIMINAR</button>
-        </div>
-      </form>
+          <!-- Guardar proyecto -->
+          <div class="mb-5 row align-items-center">
+            <div class="col-md-3"></div>
+            <div class="col-md-9">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="checkGuardar">
+                <label class="form-check-label" for="checkGuardar">Guardar automáticamente</label>
+              </div>
+            </div>
+          </div>
+
+          <!-- Botones -->
+          <div class="d-flex justify-content-between">
+            <button type="submit" class="btn-guardar">GUARDAR</button>
+            <button type="button" class="btn-eliminar">ELIMINAR</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
   </div>
 </main>
 @endsection

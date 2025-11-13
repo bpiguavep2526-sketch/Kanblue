b9ceff14b348a1607c62daedb33ff7a0e3343fe1
Eric Peralta <eperaltas2324@politecnics.barcelona>
2025-11-13 18:57:33 +0100

    arreglos

--- resources/views/tasks/taskscreen.blade.php
+++ resources/views/tasks/taskscreen.blade.php
@@ -24,49 +24,48 @@
                 <h2 class="columnTitle">BACKLOG</h2>
                 <img src="images/showlist.png" class="showList" alt="Desplegar" width="30px" height="30px">
             </div>
-            <div class="taskList" data-status="backlog">
-                @foreach ($tareas as $tarea)
-                    @if ($tarea->id_estado == 1)
-                        <div draggable="true" class="taskCard" data-status="backlog" data-task-id={{ $tarea->id_tarea }}>
-                            <div class="taskHeader">
-                                <h4>{{ $tarea->titulo }}</h4>
-                            </div>
-                            <hr
-                                style="width:100%;text-align:left;margin-left:0;border:none; height: 3px; background-color: black;">
-                            <p class="descTask">{{ $tarea->descripcion }}</p>
-                            <hr
-                                style="width:100%;text-align:left;margin-left:0;border:none; height: 3px; background-color: black;">
-                            <div class="taskDetails">
-                                <div style="font-family: 'Poppins', sans-serif;">
-                                    <img src="images/available_task.png" alt="Disponible" width="20px" height="20px"
-                                        class="d-inline-block align-text-center me-1">
-                                    @if ($tarea->id_tipus)
-                                        @foreach ($usuarios as $user)
-                                            @if ($user->id_usuario == $tarea->id_usuario)
-                                                {{ $user->username }}
-                                            @endif
-                                        @endforeach
-                                    @else
-                                        Disponible
-                                    @endif
-                                </div>
-                                @foreach ($tipostarea as $tasktype)
-                                    @if ($tasktype->id_tipus == $tarea->id_tipus)
-                                        <div class="d-flex justify-content-between">
-                                            <form action="{{ route('tasks.edit', ['task' => $tarea->id_tarea]) }}"
-                                                method="get" class="d-inline me-1">
-                                                <button class="btn btn-primary" style="margin-top: 10px">
-                                                    <img src="images/info_task.png" alt="Disponible" width="25px"
-                                                        height="25px">
-                                                </button>
-                                            </form>
-                                            <p class="tagRed">{{ $tasktype->nom }}</p>
-                                        </div>
+             <div class="taskList" data-status="backlog">
+        @foreach ($tareas as $tarea)
+            @if ($tarea->id_estado == 1)
+                <div draggable="true" class="taskCard" data-status="backlog" data-task-id={{ $tarea->id_tarea }}>
+                    <div class="taskHeader">
+                        <h4>{{ $tarea->titulo }}</h4>
+                    </div>
+                    <hr style="width:100%;text-align:left;margin-left:0;border:none; height: 3px; background-color: black;">
+                    <p class="descTask">{{ $tarea->descripcion }}</p>
+                    <hr style="width:100%;text-align:left;margin-left:0;border:none; height: 3px; background-color: black;">
+                    <div class="taskDetails">
+                        <div style="font-family: 'Poppins', sans-serif;">
+                            <img src="images/available_task.png" alt="Disponible" width="20px" height="20px"
+                                class="d-inline-block align-text-center me-1">
+                            @if ($tarea->id_tipus)
+                                @foreach ($usuarios as $user)
+                                    @if ($user->id_usuario == $tarea->id_usuario)
+                                        {{ $user->username }}
                                     @endif
                                 @endforeach
-                            </div>
+                            @else
+                                Disponible
+                            @endif
                         </div>
-            </div>
+                        @foreach ($tipostarea as $tasktype)
+                            @if ($tasktype->id_tipus == $tarea->id_tipus)
+                                <div class="d-flex justify-content-between">
+                                    <form action="{{ route('tasks.edit', ['task' => $tarea->id_tarea]) }}" method="get"
+                                        class="d-inline me-1">
+                                        <button class="btn btn-primary" style="margin-top: 10px">
+                                            <img src="images/info_task.png" alt="Disponible" width="25px" height="25px">
+                                        </button>
+                                    </form>
+                                    <p class="tagRed">{{ $tasktype->nom }}</p>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+        @endforeach
+    </div>
         </div>
         <div class="column">
             <div class="d-flex align-items-center">
@@ -227,4 +226,4 @@
         </div>
         <script src="js/tasks/tasks.js"></script>
     </div>
-@endsection
+@endsection 
